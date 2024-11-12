4c1a86a1ad867526f7b9ec969154be0ffaf19e2b
Ganga Chhetri <gangachhetry1998@gmail.com>
2024-11-12 17:08:54 +0530

    operators.je

--- add.js
+++ add.js
@@ -1,2 +1,10 @@
 // console.log(1-7)
-console.log("GANGA")
+// console.log("GANGA")
+let a=7
+b=8
+console.log(a+b)
+let x=a+b
+a=10
+b=20
+x=a*b
+console.log(x)
---
+++ oprtr.js
@@ -0,0 +1,21 @@
+// && operator example 1
+// age=20
+// if(age>5 && age>9)
+// {
+//     console.log("hello")
+// }
+
+// example 2
+// age=30
+// if(age<32 && age<31)
+// {
+//     console.log("Hey! How are you")
+// }
+
+// Logical || operator example
+
+age=23
+if( age<20 || age<25)
+{
+    console.log("Hello! How are you?")
+}
